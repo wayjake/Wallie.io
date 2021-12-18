8fc0f1c8a60523f6a6b05e5a69506e111882e064
Jake Berg <jake@xyzdigital.com>
2021-12-18 09:48:33 -0800

    wrapping the app in a wrapper
    it seems like there may be times where it would be helpful to target the full space from anywhere in the app.
    
    but if not, we could easily pop OUT of that route and into a new one to NOT include that wrapper.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import VoteLandingPage from './Vote/Landing'
+import { BrowserRouter, Outlet, Routes, Route } from 'react-router-dom'
+import VoteLandingPage, { AppWrapper } from './Vote/Landing'
 import { ViewNode, NewNode, NodesLanding } from './Nodes'
+
+function Wrapper() {
+    return (
+        <AppWrapper>
+            <Outlet />
+        </AppWrapper>
+    )
+}
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/">
+                <Route path="/" element={<Wrapper />}>
                     <Route index element={<NodesLanding />} />
                     <Route path="nodes" element={<NodesLanding />} />
                     <Route path="nodes/:id" element={<ViewNode />} />
