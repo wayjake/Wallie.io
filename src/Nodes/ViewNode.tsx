df1d466675e79a7153930f804618b341ab15ce66
Jake Berg <jake@xyzdigital.com>
2021-12-18 15:35:44 -0800

    lays out general components for the node view

--- src/Nodes/ViewNode.tsx
+++ src/Nodes/ViewNode.tsx
@@ -1,4 +1,24 @@
 import { Helmet } from 'react-helmet'
+import gun, { namespace } from '../gun'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const MessageWrapper = styled.div`
+    padding: 2rem;
+`
+const Username = styled.div`
+    font-weight: 600;
+`
+
+const Message = styled.div``
+
+const LinkWrapper = styled.div``
+
+const NodeLink = styled(Link)``
+
+const Button = styled.button`
+    font-weight: 600;
+`
 
 const ViewNode = () => {
     const titleText = ('' || 'hello world').substring(0, 50)
@@ -7,7 +27,14 @@ const ViewNode = () => {
             <Helmet>
                 <title>View Node '{titleText}'</title>
             </Helmet>
-            wth mate
+            <MessageWrapper>
+                <Username>{'username'}:</Username>
+                <Message>{'message'}</Message>
+            </MessageWrapper>
+            <LinkWrapper>
+                <NodeLink to={`/node/${'key'}`}>{'key'}</NodeLink>
+            </LinkWrapper>
+            <Button>New</Button>
         </>
     )
 }
