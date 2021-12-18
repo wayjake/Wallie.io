e47e4d47cc9cc67a811207851c26fba6c06562d8
Jake Berg <jake@xyzdigital.com>
2021-12-18 09:48:49 -0800

    because GunId tells me more than String

--- src/Nodes/index.ts
+++ src/Nodes/index.ts
@@ -1,3 +1,11 @@
 export { default as NewNode } from './NewNode'
 export { default as ViewNode } from './ViewNode'
 export { default as NodesLanding } from './NodesLanding'
+
+export type GunId = String
+
+export type DungeonNode = {
+    key: GunId
+    head: GunId
+    message: String
+}
