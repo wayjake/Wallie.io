eff6fc04d20c08ab5c38dd92f22ba698bf094a7b
Jake Berg <jake@xyzdigital.com>
2021-12-18 18:00:39 -0800

    adding a username type because it's cool

--- src/Nodes/index.ts
+++ src/Nodes/index.ts
@@ -3,9 +3,11 @@ export { default as ViewNode } from './ViewNode'
 export { default as NodesLanding } from './NodesLanding'
 
 export type GunId = String
+export type Username = String
 
 export type DungeonNode = {
     key: GunId
     head: GunId
     message: String
+    user: GunId | Username
 }
