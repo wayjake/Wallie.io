188d005e5a7b6c6915acda18b3a8607a3deb0422
Jake Berg <jake@xyzdigital.com>
2021-12-20 06:00:21 -0800

    add a back button if head exists

--- src/Nodes/ViewNode.tsx
+++ src/Nodes/ViewNode.tsx
@@ -1,15 +1,39 @@
 import { Helmet } from 'react-helmet'
 import gun, { namespace } from '../gun'
 import styled from 'styled-components'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { DungeonNode, NewSubNode } from '.'
 
 const ITEM_BORDER = `dashed blue thin`
 
+const BackSectionWrapper = styled.div`
+    padding: 1rem 2rem;
+    border: ${ITEM_BORDER};
+    display: flex;
+    justify-content: space-between;
+`
+
+const BackButton = styled.div`
+    font-weight: 800;
+    cursor: pointer;
+    color: white;
+    background-color: blue;
+    user-select: none;
+    padding: 0.3rem 0.5rem;
+    border-radius: 1rem;
+`
+const NewNode = styled(Link)`
+    color: white;
+    background-color: blue;
+    padding: 0.3rem;
+    border-radius: 1rem;
+`
+
 const MessageWrapper = styled.div`
     padding: 1rem 2rem;
     border: ${ITEM_BORDER};
+    margin: 1rem 0 0 0;
 `
 const Username = styled.div`
     padding: 1rem 1rem;
@@ -39,6 +63,7 @@ const NodeLink = styled(Link)`
 const ViewNode = () => {
     const [node, setNode] = useState<DungeonNode | undefined>()
     const { key = '' } = useParams()
+    const navigate = useNavigate()
     const keyRef = gun.get(namespace + 'node').get(key)
 
     useEffect(() => {
@@ -51,12 +76,23 @@ const ViewNode = () => {
         console.log(`i'm in view node`)
     }
 
+    const goback = () => {
+        navigate(`/node/${node?.head}`)
+    }
+
     return (
         <>
             <Helmet>
                 <title>View Node '{key.substring(0, 50)}'</title>
             </Helmet>
+            {node?.head && (
+                <BackSectionWrapper>
+                    <BackButton onClick={goback}>{'< '}</BackButton>
+                    <NewNode to="/nodes/new">New Node</NewNode>
+                </BackSectionWrapper>
+            )}
             <MessageWrapper>
+                /* @todo put date here */
                 {node?.user && <Username>@{node?.user}</Username>}
                 <Message>{node?.message}</Message>
             </MessageWrapper>
