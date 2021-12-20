a09c27239c2972e14bf59b18e0956c09870d9d44
Jake Berg <jake@xyzdigital.com>
2021-12-20 07:53:31 -0800

    new typings for nodes

--- src/Nodes/index.ts
+++ src/Nodes/index.ts
@@ -3,12 +3,19 @@ export { default as NewSubNode } from './NewSubNode'
 export { default as ViewNode } from './ViewNode'
 export { default as NodesLanding } from './NodesLanding'
 
-export type GunId = String
-export type Username = String
+export type GunId = string
+export type Username = string
+export type Unixtimestamp = string
+
+export type Directions = {
+    GunId: string
+}
 
 export type DungeonNode = {
     key: GunId
     head: GunId
-    message: String
+    message: string
+    date: Unixtimestamp
     user: GunId | Username
+    directions: Directions
 }
