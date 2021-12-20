9875ea39d8b00d5befc04595342e09583bd72af7
Jake Berg <jake@xyzdigital.com>
2021-12-20 07:53:41 -0800

    handle directions navigation

--- src/Nodes/ViewNode.tsx
+++ src/Nodes/ViewNode.tsx
@@ -2,8 +2,8 @@ import { Helmet } from 'react-helmet'
 import gun, { namespace } from '../gun'
 import styled from 'styled-components'
 import { Link, useParams, useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import { DungeonNode, NewSubNode } from '.'
+import { useEffect, useMemo, useState } from 'react'
+import { Directions, DungeonNode, NewSubNode } from '.'
 
 const ITEM_BORDER = `dashed blue thin`
 
@@ -35,9 +35,19 @@ const MessageWrapper = styled.div`
     border: ${ITEM_BORDER};
     margin: 1rem 0 0 0;
 `
+const MessageTop = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
 const Username = styled.div`
     padding: 1rem 1rem;
     font-weight: 600;
+    border: ${ITEM_BORDER};
+`
+const MessageDate = styled.div`
+    padding: 1rem 1rem;
     border: ${ITEM_BORDER};
 `
 
@@ -58,18 +68,34 @@ const LinkWrapper = styled.div`
 const NodeLink = styled(Link)`
     padding: 1rem 1rem;
     border: ${ITEM_BORDER};
+    margin-top: 0.5rem;
 `
 
 const ViewNode = () => {
     const [node, setNode] = useState<DungeonNode | undefined>()
+    const [directions, setDirections] = useState<any>({})
     const { key = '' } = useParams()
     const navigate = useNavigate()
-    const keyRef = gun.get(namespace + 'node').get(key)
 
     useEffect(() => {
-        keyRef.once((node: DungeonNode | any = {}) => {
-            setNode(node)
-        })
+        gun.get(namespace + 'node')
+            .get(key)
+            .once((node: DungeonNode | any = {}) => {
+                setNode(node)
+            })
+    }, [key])
+
+    useEffect(() => {
+        setDirections({})
+        gun.get(namespace + 'node')
+            .get(key)
+            .get('directions')
+            .map()
+            .once((message: any, key: any) => {
+                setDirections((prev: any) => {
+                    return { ...prev, ...{ [key]: message } }
+                })
+            })
     }, [key])
 
     const nodeAdded = () => {
@@ -80,24 +106,40 @@ const ViewNode = () => {
         navigate(`/node/${node?.head}`)
     }
 
+    const dateFormatted = useMemo(() => {
+        if (!node?.date) return ''
+        const date = new Date(node?.date)
+
+        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+    }, [node?.date])
+
     return (
         <>
             <Helmet>
                 <title>View Node '{key.substring(0, 50)}'</title>
             </Helmet>
-            {node?.head && (
-                <BackSectionWrapper>
-                    <BackButton onClick={goback}>{'< '}</BackButton>
-                    <NewNode to="/nodes/new">New Node</NewNode>
-                </BackSectionWrapper>
-            )}
+
+            <BackSectionWrapper>
+                {node?.head && <BackButton onClick={goback}>{'< '}</BackButton>}
+                {!node?.head && <div>&nbsp;</div>}
+                <NewNode to="/nodes/new">New Parent</NewNode>
+            </BackSectionWrapper>
+
             <MessageWrapper>
-                /* @todo put date here */
-                {node?.user && <Username>@{node?.user}</Username>}
+                <MessageTop>
+                    {node?.user && <Username>@{node?.user}</Username>}
+                    <MessageDate>{dateFormatted}</MessageDate>
+                </MessageTop>
                 <Message>{node?.message}</Message>
             </MessageWrapper>
             <LinkWrapper>
-                <NodeLink to={`/node/${'key'}`}>{'key'}</NodeLink>
+                {Object.keys(directions).map((key: string) => {
+                    return (
+                        <NodeLink to={`/node/${key}`} key={key}>
+                            {directions[key]}
+                        </NodeLink>
+                    )
+                })}
                 <NewSubNode head={key} nodeAdded={nodeAdded} />
             </LinkWrapper>
         </>
