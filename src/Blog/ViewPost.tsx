19cfe5939134a7cce36625cff66cec33457d925f
Jake Berg <jake@xyzdigital.com>
2022-02-23 17:18:23 -0800

    Adds a wrapper to the post view

--- src/Blog/ViewPost.tsx
+++ src/Blog/ViewPost.tsx
@@ -1,7 +1,12 @@
 import { Helmet } from 'react-helmet'
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 import useListen from '../GunApi/useListen'
 import { createMarkup } from '../utils'
+
+const PostStyled = styled.div`
+    max-width: 520px;
+`
 
 const ViewPost = () => {
     const { key = '' } = useParams()
@@ -13,7 +18,7 @@ const ViewPost = () => {
             <Helmet>
                 <title>I am a post!</title>
             </Helmet>
-            <div dangerouslySetInnerHTML={createMarkup(post)}></div>
+            <PostStyled dangerouslySetInnerHTML={createMarkup(post)} />
         </div>
     )
 }
