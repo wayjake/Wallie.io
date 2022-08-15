84383b19347098806fd0be7684ce4c4a5915778d
Jake Berg <jake@xyzdigital.com>
2022-08-15 15:16:12 -0700

    Stops gun from saving to disk (rad) changes namespace

--- server/gun.js
+++ server/gun.js
@@ -1,13 +1,16 @@
 const GUN = require('gun')
 
-const gun = GUN([
-    'https://etogun.glitch.me/gun',
-    'https://relay.peer.ooo/gun',
-    'https://gun-ams1.maddiex.wtf/gun',
-    'https://gun-manhattan.herokuapp.com',
-])
+const gun = GUN({
+    radisk: false,
+    peers: [
+        'https://etogun.glitch.me/gun',
+        'https://relay.peer.ooo/gun',
+        'https://gun-ams1.maddiex.wtf/gun',
+        'https://gun-manhattan.herokuapp.com',
+    ],
+})
 
 module.exports = {
     gun,
-    namespace: `wallie`,
+    namespace: `wallie2.0`,
 }
