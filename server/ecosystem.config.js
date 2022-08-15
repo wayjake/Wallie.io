e89b77077e3d546454a27051f281bdeb46f5e1de
Jake Berg <jake@xyzdigital.com>
2022-08-15 15:16:24 -0700

    Adds ecosystem for pm2 management

---
+++ server/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'wallie',
+            args: '3358',
+            script: './index.js',
+            watch: false,
+        },
+    ],
+}
