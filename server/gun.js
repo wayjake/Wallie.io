4afe60e3282cf509d59d02ac8229904d61080475
Jake Berg <jake@xyzdigital.com>
2022-09-19 12:45:01 -0700

    Cut back down to just the default peer

--- server/gun.js
+++ server/gun.js
@@ -2,7 +2,10 @@ const GUN = require('gun')
 
 const gun = GUN({
     radisk: false,
-    peers: ['https://relay.peer.ooo/gun', 'https://gun.4d2.io/gun'],
+    peers: [
+        'https://relay.peer.ooo/gun',
+        // 'https://gun.4d2.io/gun'
+    ],
 })
 
 module.exports = {
--- src/gun.ts
+++ src/gun.ts
@@ -3,7 +3,10 @@ import GUN from 'gun'
 const gun = GUN({
     localStorage: false,
     // radisk: false,
-    peers: ['https://relay.peer.ooo/gun', 'https://gun.4d2.io/gun'],
+    peers: [
+        'https://relay.peer.ooo/gun',
+        // 'https://gun.4d2.io/gun'
+    ],
 })
 
 export default gun
