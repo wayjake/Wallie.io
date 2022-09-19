2c94aa4c5821a6f13fd0ef371f914d07caabce88
Jake Berg <jake@xyzdigital.com>
2022-09-19 13:44:36 -0700

    Add an interval to send a heartbeat update

--- src/gun.ts
+++ src/gun.ts
@@ -12,3 +12,14 @@ const gun = GUN({
 export default gun
 
 export const namespace = `wallie2.0`
+
+// every 15 minutes send an update to make sure we're still connected
+setInterval(() => {
+    gun.get(`${namespace}/heartbeat`).put(
+        { time: new Date().getTime() },
+        (awk) => {
+            console.log(awk)
+            console.log(`heartbeat performed`)
+        }
+    )
+}, 10 * 60 * 1000)
