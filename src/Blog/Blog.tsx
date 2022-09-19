d997aadac3adbc76bb0d0e256669ddb459a31068
Jake Berg <jake@xyzdigital.com>
2022-09-19 12:47:18 -0700

    Refactor get calls to listen to each post independently

--- src/Blog/Blog.tsx
+++ src/Blog/Blog.tsx
@@ -4,28 +4,42 @@ import { createMarkup } from '../utils'
 import usePostClicked from './usePostClicked'
 import { PostStyled } from './ViewPost'
 
-const ViewPostList = () => {
-    const posts = useListen(undefined, 'post', false)
+const GetPost = ({ id }) => {
+    const post = useListen(id, 'post', true)
     const postClicked = usePostClicked()
+
+    if (!post) return null
+
+    return (
+        <PostStyled
+            key={post.key}
+            onClick={(event) => {
+                postClicked(post.key, {
+                    metaKey: event.metaKey,
+                    altKey: event.altKey,
+                })
+            }}
+            dangerouslySetInnerHTML={createMarkup(post.content)}
+        />
+    )
+}
+
+const ViewPostList = () => {
+    const posts = useListen(undefined, 'post', true)
 
     useEffect(() => {
         document.title = `Blog`
     }, [])
 
+    if (!posts) return null
+
     return (
         <div>
-            {posts.map((post) => (
-                <PostStyled
-                    key={post.key}
-                    onClick={(event) => {
-                        postClicked(post.key, {
-                            metaKey: event.metaKey,
-                            altKey: event.altKey,
-                        })
-                    }}
-                    dangerouslySetInnerHTML={createMarkup(post.content)}
-                />
-            ))}
+            {Object.keys(posts).map((key) => {
+                if (key === 'key') return null
+                if (key === '_') return null
+                return <GetPost id={key} key={key} />
+            })}
         </div>
     )
 }
