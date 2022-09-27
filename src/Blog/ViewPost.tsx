df94b9a125fcdbf66456120f241f503a70db578a
Jake Berg <jake@xyzdigital.com>
2022-09-27 10:28:54 -0700

    Improves handling of preloaded data

--- src/Blog/ViewPost.tsx
+++ src/Blog/ViewPost.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import useListen from '../GunApi/useListen'
@@ -25,21 +25,43 @@ const colors = ['#3ed3c9', '#f8633c', '#5970cd', '#f4d400']
 
 const ViewPost: React.FC = () => {
     const { key = '' } = useParams()
+    const [tempContent, setTempContent] = useState<string>(null!)
     const post = useListen(key, 'post', true)
     const postClicked = usePostClicked()
     const borderColor: string = useMemo(
         () => getRandomFromArray(colors),
         [post?.title]
     )
+
+    const getPreloadData = (): void => {
+        const content: HTMLMetaElement | null = document.querySelector(
+            'meta[name="content"]'
+        )
+        if (!content?.content) {
+            return
+        }
+        setTempContent(decodeURI(content?.content))
+    }
+
     useEffect(() => {
-        document.title = post?.title || 'Title is missing'
+        const description: HTMLMetaElement | null = document.querySelector(
+            'meta[name="description"]'
+        )
+        if (description?.content === `__META_OG_DESCRIPTION__`) {
+            // if the SSR is in effect, this field will be altered from the template html file
+            document.title = post?.title || 'Title is missing'
+        } else {
+            getPreloadData()
+        }
     }, [post?.title])
 
     return (
         <div>
             <PostStyled
                 key={post?.key}
-                dangerouslySetInnerHTML={createMarkup(post?.content)}
+                dangerouslySetInnerHTML={createMarkup(
+                    post?.content || tempContent
+                )}
                 borderColor={borderColor}
                 onClick={(event) => {
                     postClicked(key, {
