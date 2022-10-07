efaf99e3e59b1f2f2ae7ec9bea2d82277dbd27b6
Jake Berg <jake@xyzdigital.com>
2022-10-07 16:04:59 -0700

    Remove non-query use case

--- src/GunApi/useListen.ts
+++ src/GunApi/useListen.ts
@@ -3,44 +3,40 @@ import { isConstructorDeclaration } from 'typescript'
 import gun, { namespace } from '../gun'
 
 const useListen = (
-    query: string | undefined | null,
-    model: string = 'node',
-    single: boolean = false
+   query: string | undefined | null,
+   model: string = 'node',
+   single: boolean = false
 ) => {
-    const [nodes, setNodes] = useState<any[]>([])
+   const [nodes, setNodes] = useState<any[]>([])
 
-    const setNodesCallback = (newNode: any = {}, key) => {
-        if (single && query !== key) {
-            console.log(`query not in sync with result`, query, key)
-        }
+   const setNodesCallback = (newNode: any = {}, key) => {
+      if (single && query !== key) {
+         console.log(`query not in sync with result`, query, key)
+      }
 
-        setNodes((nodes) => {
-            const filteredNodes = nodes.filter((node) => node.key !== key)
-            if (!newNode) {
-                return filteredNodes
-            }
-            return [...filteredNodes, { ...newNode, key }]
-        })
-    }
+      setNodes((nodes) => {
+         const filteredNodes = nodes.filter((node) => node.key !== key)
+         if (!newNode) {
+            return filteredNodes
+         }
+         return [...filteredNodes, { ...newNode, key }]
+      })
+   }
 
-    useEffect(() => {
-        setNodes([])
-        if (query) {
-            const chain = gun
-                .get(`${namespace}/${model}`)
-                .get(query)
-                .on(setNodesCallback)
-            return () => chain.off()
-        }
-        const chain = gun.get(`${namespace}/${model}`).on(setNodesCallback)
-        return () => {
-            chain.off()
-            return
-        }
-    }, [query])
+   useEffect(() => {
+      setNodes([])
+      if (!query) {
+         return () => {}
+      }
+      const chain = gun
+         .get(`${namespace}/${model}`)
+         .get(query)
+         .on(setNodesCallback)
+      return () => chain.off()
+   }, [query])
 
-    if (single) return nodes[0]
-    return nodes
+   if (single) return nodes[0]
+   return nodes
 }
 
 export default useListen
