4d9606f3bc566904dafd6503c87772ee559efc1f
Jake Berg <jake@xyzdigital.com>
2022-11-08 10:49:43 -0800

    Changing to peer.wallie.io
    Also includes some formatting changes from Prettier.

--- src/gun.ts
+++ src/gun.ts
@@ -1,12 +1,9 @@
 import GUN from 'gun'
 
 const gun = GUN({
-    localStorage: false,
-    // radisk: false,
-    peers: [
-        'https://relay.peer.ooo/gun',
-        // 'https://gun.4d2.io/gun'
-    ],
+   localStorage: false,
+   // radisk: false,
+   peers: ['https://peer.wallie.io/gun'],
 })
 
 export default gun
@@ -15,11 +12,11 @@ export const namespace = `wallie2.0`
 
 // every 15 minutes send an update to make sure we're still connected
 setInterval(() => {
-    gun.get(`${namespace}/heartbeat`).put(
-        { time: new Date().getTime() },
-        (awk) => {
-            console.log(awk)
-            console.log(`heartbeat performed`)
-        }
-    )
+   gun.get(`${namespace}/heartbeat`).put(
+      { time: new Date().getTime() },
+      (awk) => {
+         console.log(awk)
+         console.log(`heartbeat performed`)
+      }
+   )
 }, 10 * 60 * 1000)
