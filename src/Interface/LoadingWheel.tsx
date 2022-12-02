e82cb8aaab0762d34c3e8cb5acc85448be2177d3
Jake Berg <jake@xyzdigital.com>
2022-12-02 08:57:34 -0800

    Passes down className to loading bar

--- src/Interface/LoadingWheel.tsx
+++ src/Interface/LoadingWheel.tsx
@@ -1,31 +1,31 @@
 import styled from 'styled-components'
-import { useEffect, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 const animation_time = 100
 const delimiter = '.'
 
 const StyledLoadingWheel = styled.div`
-    align-self: center;
-    width: 1rem;
-    padding: 0 0.6rem 0 0;
+   align-self: center;
+   width: 1rem;
+   padding: 0 0.6rem 0 0;
 `
 
-const LoadingWheel = () => {
-    const [text, setText] = useState<string>(delimiter)
+const LoadingWheel: FC<{ className?: string }> = ({ className }) => {
+   const [text, setText] = useState<string>(delimiter)
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setText((curVal: string) => {
-                if (curVal.lastIndexOf(delimiter) > 1) return delimiter
-                return curVal + delimiter
-            })
-        }, animation_time)
-        return () => {
-            clearInterval(intervalId)
-        }
-    }, [])
+   useEffect(() => {
+      const intervalId = setInterval(() => {
+         setText((curVal: string) => {
+            if (curVal.lastIndexOf(delimiter) > 1) return delimiter
+            return curVal + delimiter
+         })
+      }, animation_time)
+      return () => {
+         clearInterval(intervalId)
+      }
+   }, [])
 
-    return <StyledLoadingWheel>{text}</StyledLoadingWheel>
+   return <StyledLoadingWheel className={className}>{text}</StyledLoadingWheel>
 }
 
 export default LoadingWheel
