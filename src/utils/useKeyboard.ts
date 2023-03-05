9556b01960d022a468a888aaa740abe84c6543c7
Jake Berg <jake@xyzdigital.com>
2023-03-04 18:43:29 -0800

    reformatted with prettier

--- src/utils/useKeyboard.ts
+++ src/utils/useKeyboard.ts
@@ -2,36 +2,36 @@ import { useState, useEffect } from 'react'
 import { userIsWithinInput } from '.'
 
 const useKeyboard = (targetKey: string[]): string | undefined => {
-    const [keyPressed, setKeyPressed] = useState<string | undefined>('')
+   const [keyPressed, setKeyPressed] = useState<string | undefined>('')
 
-    const downHandler = ({ key }: KeyboardEvent): void => {
-        if (userIsWithinInput()) {
-            return
-        }
-        if (targetKey.includes(key)) {
-            setKeyPressed(key)
-        }
-    }
+   const downHandler = ({ key }: KeyboardEvent): void => {
+      if (userIsWithinInput()) {
+         return
+      }
+      if (targetKey.includes(key)) {
+         setKeyPressed(key)
+      }
+   }
 
-    const upHandler = ({ key }: KeyboardEvent): void => {
-        if (userIsWithinInput()) {
-            return
-        }
-        if (targetKey.includes(key)) {
-            setKeyPressed(undefined)
-        }
-    }
+   const upHandler = ({ key }: KeyboardEvent): void => {
+      if (userIsWithinInput()) {
+         return
+      }
+      if (targetKey.includes(key)) {
+         setKeyPressed(undefined)
+      }
+   }
 
-    useEffect(() => {
-        window.addEventListener('keydown', downHandler)
-        window.addEventListener('keyup', upHandler)
-        return () => {
-            window.removeEventListener('keydown', downHandler)
-            window.removeEventListener('keyup', upHandler)
-        }
-    }, [])
+   useEffect(() => {
+      window.addEventListener('keydown', downHandler)
+      window.addEventListener('keyup', upHandler)
+      return () => {
+         window.removeEventListener('keydown', downHandler)
+         window.removeEventListener('keyup', upHandler)
+      }
+   }, [])
 
-    return keyPressed
+   return keyPressed
 }
 
 export default useKeyboard
